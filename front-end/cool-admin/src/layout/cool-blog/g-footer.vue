cdce489f94754b1127029f53ed00dae606007d25
luxiaotao1123 <1341870251@qq.com>
2017-12-26 17:18:57 +0800

    dsa

--- front-end/cool-admin/src/layout/cool-blog/g-footer.vue
+++ front-end/cool-admin/src/layout/cool-blog/g-footer.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="global-footer">
-        <h2 class="cool-logo"><img src="" width="170px"></h2>
         <div class="footer-box">
             <p> Copyright © 2017 CoolBlog Powered by CoolBoy </p>
         </div>
@@ -10,12 +9,40 @@
 
 <style scoped>
 .footer-box{
-    width:1000px;
+    width:100%;
     margin: 0 auto
 }
 .footer-box p{
     text-align: center;
-    padding-bottom: 20px
+    padding-bottom: 20px;
+    padding-top: 10px
+}
+@media screen and (max-width: 1024px)  {
+    .article-cards{
+        width: 90%;
+        margin: 0px auto
+    }
+    .article-card{
+        /* width: 100%; */
+        overflow: hidden;   
+    }
+   
+}
+@media screen and (max-width: 420px)  {
+ .article-card .blog-img>img{
+  height: 230px;
+transition: all 1s ease;
+    }
+    .article-title{
+            top: 135px;
+            transition: all 1s ease;
+    }
+.article-card .blog-img a>img{
+    height: 231px;
+}
+.article-title .title-box .title-white a{
+       font-size: 17px;
+}
 }
 </style>
   
